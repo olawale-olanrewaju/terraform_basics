a84cfdbeecf5efcb4896f733a44389fb4f8954e6
Olawale Olanrewaju <olanrewajume@yahoo.com>
2024-08-22 19:58:17 +0100

    Format configurations. Get availability zone as data source

--- main.tf
+++ main.tf
@@ -8,16 +8,20 @@ resource "aws_vpc" "dev-vpc" {
   cidr_block = var.vpc-cidr-block
 
   tags = {
-    Name: var.environment
+    Name : var.environment
   }
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_subnet" "dev-subnet-1" {
-  vpc_id = aws_vpc.dev-vpc.id
-  cidr_block = var.subnet-cidr-block
-  availability_zone = "us-east-1a"
+  vpc_id            = aws_vpc.dev-vpc.id
+  cidr_block        = var.subnet-cidr-block
+  availability_zone = data.aws_availability_zones.available.names[0]
 
   tags = {
-    Name: var.environment
+    Name : var.environment
   }
 }
--- variable.tf
+++ variable.tf
@@ -1,14 +1,14 @@
 variable "subnet-cidr-block" {
   description = "CIDR block for the subnet"
-  type = string
+  type        = string
 }
 
 variable "vpc-cidr-block" {
   description = "CIDR block for the vpc"
-  type = string
+  type        = string
 }
 
 variable "environment" {
   description = "Deployment environment"
-  type = string
+  type        = string
 }
